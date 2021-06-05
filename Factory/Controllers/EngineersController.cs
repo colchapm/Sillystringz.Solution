6d22da08794c97c9ac7d46334c2ed253023d8f96
Collin Chapman <cchap14@gmail.com>
2021-06-05 09:37:32 -0700

    create index route and view for Engineers

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db =db;
+    }
+
+    public ActionResult Index()
+    {
+      IEnumerable<Engineer> sortedEngineers = _db.Engineers.OrderBy(engineer => engineer.EngineerName);
+      return View(sortedEngineers.ToList());
+    }
+  }
+}
