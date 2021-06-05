e619d8fda06d72e169f4c988ef26f2ab2620e48c
Collin Chapman <cchap14@gmail.com>
2021-06-05 09:15:52 -0700

    create models and join entities and run initial database migration

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+
+    public string EngineerName { get; set; }
+
+    public string EngineerTitle { get; set; }
+
+    public DateTime HireDate { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+  }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+
+    public string MachineName { get; set; }
+
+    public DateTime InstallDate { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+
+    }
+}
