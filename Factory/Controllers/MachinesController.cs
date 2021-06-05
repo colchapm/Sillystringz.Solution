ba2a28d1c7c01b2166a59540729e2057b0433448
Collin Chapman <cchap14@gmail.com>
2021-06-05 11:19:25 -0700

    add index route and view for Machine model

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      IEnumerable<Machine> sortedMachines = _db.Machines.OrderBy(machine => machine.MachineName);
+      return View(sortedMachines.ToList());
+    }
+  }
+}
