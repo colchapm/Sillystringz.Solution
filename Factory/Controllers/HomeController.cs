e74f2823fc1a20857a0b0e7569c107d80ce9eaa5
Collin Chapman <cchap14@gmail.com>
2021-06-05 09:22:41 -0700

    add splash route and page

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+
+  }
+}
