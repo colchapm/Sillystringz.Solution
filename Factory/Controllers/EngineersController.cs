6bb8a88a79420e672611985683fb4d32a5a00431
Collin Chapman <cchap14@gmail.com>
2021-06-05 11:10:00 -0700

    create DeleteMachine route in Engineer controller

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -95,6 +95,15 @@ namespace Factory.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpPost]
+
+    public ActionResult DeleteMachine(int joinId)
+    {
+      var joinEntry = _db.EngineerMachine.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachine.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.EngineerId });
+    }
 
   }
 }
